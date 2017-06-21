e0b43582c6624f5b663c80061ee7c5d6dacb814e
Alec Montalbano <alec.montalbano@gmail.com>
2017-06-20 20:19:10 -0400

    created cell constructor function and set styling properties of cell inside of a functoin stored in a variable and return its contents as to allow it to be easily manipulated with js.

---
+++ public/js/cell.js
@@ -0,0 +1,18 @@
+/*jshint esversion: 6*/
+
+//Constructor function
+function Cell(width, height, color){
+  this.width = width || 100;
+  this.height = height || 100;
+  this.color = color || 'red';
+}
+
+Cell.prototype.toHtml = function () {
+  const div = document.createElement('div');
+  div.style.height = `${this.height}px`;
+  div.style.display = 'inline-block';
+  div.style.width = `${this.width}px`;
+  div.style.border = '0.25px solid black';
+  div.style.backgroundColor = this.color;
+  return div;
+};
